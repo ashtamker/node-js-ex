c5b43a98fe6ae9f43a2651070313927ce49d1b05
Or Ashtamker <ashi@MacBook-Pro.local>
2021-04-11 15:26:41 +0300

    add 2-1

--- note/main.js
+++ note/main.js
@@ -1,5 +1,16 @@
-const fs = require('fs')
+const fs = require('fs');
 
 
 fs.writeFileSync('new.txt','first lesson in nodejs');
 fs.copyFileSync('new.txt', 'copyNew.txt');
+fs.rename('copyNew.txt', 'setNewName.txt', (err) => {
+    if (err) throw err;
+    console.log('Rename complete!');
+  });
+// 
+const list = () => {
+    const files = fs.readdirSync('./');
+    console.log(files);
+}
+
+list();
