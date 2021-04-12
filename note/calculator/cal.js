ad7e6b83315fd21ce7d8a99fbc4cbe3b01253349
Or Ashtamker <ashi@MacBook-Pro.local>
2021-04-12 10:10:05 +0300

    add 4-1

--- note/calculator/cal.js
+++ note/calculator/cal.js
@@ -1,19 +1,59 @@
-const yargs = require('yargs');
+// const yargs = require('yargs');
 
-yargs.command({
-    command: 'add',
-    builder: {
-        title: {
-            describe: 'Add action',
-            type: 'string',
-        },
-        body: {
-            describe: 'The sum is:',
-            type: 'number',
-        }, 
-    },
-    handler: function (a,b) {
-        let sum = a + b;
-        console.log('body:' + sum);
-    }
-})
+// yargs.command({
+//     command: 'cal',
+//     describe: 'Simple calculator',
+//     builder: {
+//         add: {
+//             describe: 'sum 2 num',
+//             type: 'number',
+//         },
+//         sub: {
+//             describe: 'sub the second num from the first',
+//             type: 'number',
+//         }
+//     },
+
+//     handler: function (a,b) {
+//         let sum =  a + b;
+//         let sub = a - b;
+//         console.log('sum:' + sum);
+//         console.log('sub' + sub);
+//     }
+// })
+
+
+
+// yargs.parse();
+// // console.log(yargs.argv);
+
+const summation = (a, b) =>  a + b;
+const subtraction = (a, b) =>  a - b;
+const multiply = (a, b) => a * b;
+const division = (a, b) =>  a / b;
+
+const yargs = require("yargs");
+
+ 
+ 
+const action = process.argv[2];
+const a = yargs.argv.a;
+const b = yargs.argv.b;
+ 
+let result = "";
+ 
+if ( action === "sum") {
+  result = summation(a, b);
+} 
+else if (action === "sub") {
+  result = subtraction(a, b);
+}
+else if (action === "multi") {
+    result = multiply(a, b);
+}
+else if (action === "div") {
+    result = division(a, b);
+}
+
+ 
+console.log(`The ${action} of ${a} , ${b} is: ${result}`)
