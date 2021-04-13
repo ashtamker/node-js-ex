865c5c966bc507790d225ea219f9d3e66b025042
Or Ashtamker <ashi@MacBook-Pro.local>
2021-04-13 14:03:23 +0300

    /

--- apiReq/index.js
+++ apiReq/index.js
@@ -1,1 +1,23 @@
 const request = require('request');
+const axios = require("axios");
+
+
+const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita';
+
+request({ url: url, json: true }, (error, response, body) => {
+    if (error) {
+      console.error("error:", error);
+    } 
+    else {
+      console.log(body.strDrink);
+}
+  });
+
+
+  axios.get(url)
+  .then(function (response) {
+    console.log(response.strDrink);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
