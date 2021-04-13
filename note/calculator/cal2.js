5fb9ba4ff568df474b0467c177ebf1025c17339b
Or Ashtamker <ashi@MacBook-Pro.local>
2021-04-13 13:27:04 +0300

    add 7-1

--- note/calculator/cal2.js
+++ note/calculator/cal2.js
@@ -3,3 +3,17 @@ const { require } = require("yargs");
 const yargs = require('yargs');
 
 
+yargs.command({
+    command: ' pow',
+    describe: 'power the number',
+    builder: {
+        x: {
+            type: 'integer',
+
+        },
+
+    },
+    handler: function(){
+        
+    }
+})
