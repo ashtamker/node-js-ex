8f40cbba748458d4cda3c80aea32cee2eeb8641a
Or Ashtamker <ashi@MacBook-Pro.local>
2021-04-14 01:00:32 +0300

    add 9-2

---
+++ pure-nodec9-2/index.js
@@ -0,0 +1,26 @@
+const http = require('http');
+const fs = require('fs');
+const url = require('url');
+const port = 5000;
+const users =('./users.json');
+
+const server =http.createServer((req, res) => {
+    if(req.url === '/'){
+        renderHTML('./index.html', res);
+    }
+    else if(req.url === '/raw-html'){
+        res.write('<h1>Welcome</h1>')
+    }
+    else if(req.url === '/users'){
+        res.writeHead(200, {'content-Type': application/json});
+        res.write(JSON.stringify(users.users));
+        res.end();
+    }
+    else{
+        res.writeHead(404);
+        res.write('not found');
+        res.end();
+    }
+})
+
+server.listen(port, () => console.log('running on port ' + port));
