6d685c88b825f72f9245378108fa4c9314e8c69f
Or Ashtamker <ashi@MacBook-Pro.local>
2021-04-14 10:50:11 +0300

    validation

--- server/worker.routes.js
+++ server/worker.routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const workerJson = require('./worker.json');
+const workers = workerJson.workers;
 const worker = [
     {
         id: 0,
@@ -10,32 +11,48 @@ const worker = [
     {
         id: 1,
         name: "Toni Kroos",
-        isActive: false
+        isActive: true
     },
     {
         id: 2,
-        name: "Luka Mudrich",
-        isActive: false
+        name: "Federico Valverde",
+        isActive: true
+    },
+    {
+        id: 3,
+        name: "Luka Modric",
+        isActive: true
     }
 ]
 
 router.get('/',(req,res)=>{
     return res.status(200).json({workerJson: workerJson})
 }).get('/:id',(req,res)=>{
-    return res.status(200).json({workerJson : workerJson[req.params.id]})
+    return res.status(200)({worker : worker.map(id => worker[id])})
 }).post('/',(req,res)=>{
-    user.push({
-        id: req.body.id,
+    if(!req.body.name){
+        return res.status(204).json({error: 'enter name'})
+    }
+    // let res = worker.find((i) => {
+    //     return i.id == id
+    // })
+    // else if(res){
+    //     return res.status(204).json({error: 'worker exist in db'})
+    // }
+    
+    workers.push({
+        id: worker.length,
         name: req.body.name,
-        capsule: req.body.capsule
+        isActive: false
     })
     return res.send(req.body)
-}).put('/:id',(req, res)=>{
-    const {id} = req.params;
-    const {} = req.body;
-    user[id].capsule = capsule
-    res.send(yes)
-       
 })
+// .put('/:id',(req, res)=>{
+//     const {id} = req.params;
+//     const {} = req.body;
+//     user[id].isActive = false
+//     res.send(yes)
+       
+// })
 
 module.exports = router;
